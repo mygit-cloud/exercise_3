1a5c9bbaad0c76d102a79210c094f9e64744c8fb
yunerliu <liuyunchenyuner@gmail.com>
2022-02-02 15:16:45 -0600

    add db file with id auto increment

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -12,3 +12,19 @@ create table comments (
   author VARCHAR(30),
   FOREIGN KEY(post_id) REFERENCES posts(id)
 );
+
+-- -- id AUTOINCREMENT version
+-- create table posts (
+--   id INTEGER PRIMARY KEY AUTOINCREMENT,
+--   slug VARCHAR(30) NOT NULL,
+--   title VARCHAR(255) NOT NULL,
+--   body TEXT
+-- );
+
+-- create table comments (
+--   id INTEGER PRIMARY KEY AUTOINCREMENT,
+--   post_id INT,
+--   body TEXT,
+--   author VARCHAR(30),
+--   FOREIGN KEY(post_id) REFERENCES posts(id)
+-- );
